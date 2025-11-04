10fb621f24f247bccf527cb71f4689ed22dda80b
Isac Chuab <isachuab@gmail.com>
2025-11-04 04:29:37 -0300

    feat: 🎸 retirada de codigo não utilizado

--- packages/front/src/pages/login/index.tsx
+++ packages/front/src/pages/login/index.tsx
@@ -1,18 +1,13 @@
-import { Button, theme } from 'antd';
+import { Button } from 'antd';
 import { useTheme } from '../../hooks/theme';
 
 const Login = () => {
   const { mode, setMode } = useTheme();
-  const { token } = theme.useToken();
 
   return (
     <div>
       <h1 className="text-lime-900 text-6xl">Login</h1>
-      <Button
-        type="default"
-        // style={{ backgroundColor: token.colorError }}
-        onClick={() => setMode(mode === 'light' ? 'dark' : 'light')}
-      >
+      <Button type="primary" onClick={() => setMode(mode === 'light' ? 'dark' : 'light')}>
         {mode === 'light' ? 'Dark Mode' : 'Light Mode'}
       </Button>
     </div>
