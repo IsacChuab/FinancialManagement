8cd3b93f508c3810f8d44f5386c6789c955f1efd
Isac Chuab <isachuab@gmail.com>
2026-02-05 15:24:45 -0300

    feat: 🎸 testando o build

--- apps/api/src/lib/EmailSender.ts
+++ apps/api/src/lib/EmailSender.ts
@@ -38,7 +38,7 @@ class EmailSender {
     await this.client.emails.send({
       from: this.from,
       to,
-      subject: 'Recuperação de senha',
+      subject: 'Recovery Your Password',
       html,
     });
   }
