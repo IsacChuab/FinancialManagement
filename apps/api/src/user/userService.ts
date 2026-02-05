cd7281715bfa64e54b1dabf85fc0c3e502f75d84
Isac Chuab <isachuab@gmail.com>
2026-02-05 02:59:41 -0300

    test: 💍 2 test api

--- apps/api/src/user/userService.ts
+++ apps/api/src/user/userService.ts
@@ -8,7 +8,7 @@ import EmailSender from '../lib/EmailSender.js';
 class UserService {
   public async createUser({ email, newPassword }: CreateUserInput) {
     const user = await userRepository.findByEmail(email);
-    console.log('1 test api');
+    console.log('2 test api');
 
     if (user) {
       throw new Error('User already exists');
