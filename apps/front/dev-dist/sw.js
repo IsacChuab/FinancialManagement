6d986c9fbfb6553d1a1d3f53db647d8c49aababd
Isac Chuab <isachuab@gmail.com>
2026-08-17 18:21:41 -0300

    fix: 🐛 fix theme login layout

--- apps/front/dev-dist/sw.js
+++ apps/front/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-bde10350'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.vb9doobtp"
+    "revision": "0.uqql6kl04bo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
